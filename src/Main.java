421373923610aa6260ecd476b3a370d98ec5078b
bedriadem85-rgb <bedriadem85@gmail.com>
2026-04-04 12:45:07 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 
-        
-        // --- TODO: Step 4 ---
-  
-public class Main {
+        public class Main {
     public static void main(String[] args) {
 
         // Create Student objects
-        Student student1 = new Student("Alice", 101);
-        Student student2 = new Student("Bob", 102);
+        Student student1 = new Student("Ali", 101);
+        Student student2 = new Student("Sara", 102);
 
-        // Print student names
+        // Print names
         System.out.println("Student 1 Name: " + student1.getName());
         System.out.println("Student 2 Name: " + student2.getName());
     }
 }
+      
