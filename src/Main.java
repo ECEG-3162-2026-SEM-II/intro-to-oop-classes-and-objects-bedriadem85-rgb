38345fc315f4681f52a96c2975641c3348e7ec27
bedriadem85-rgb <bedriadem85@gmail.com>
2026-04-13 00:41:42 -0700

    Update Main.java assignment

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create Student objects
+        Student student1 = new Student("Ali", 101);
+        Student student2 = new Student("Sara", 102);
 
-        public class Student {
-    // Private fields (Encapsulation)
-    private String name;
-    private int studentId;
-
-    // Constructor
-    public Student(String name, int studentId) {
-        this.name = name;
-        this.studentId = studentId;
-    }
-
-    // Getter for name
-    public String getName() {
-        return name;
-    }
-
-    // Setter for name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter for studentId
-    public int getStudentId() {
-        return studentId;
-    }
-
-    // Setter for studentId
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
+        // Print student names
+        System.out.println("Student 1: " + student1.getName());
+        System.out.println("Student 2: " + student2.getName());
     }
 }
+       
       
